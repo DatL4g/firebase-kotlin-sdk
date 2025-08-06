bca37e1c7048fac52ad3af2a269a75b447415688
Jeff <jeff.retz08@gmail.com>
2025-08-06 22:35:53 +0200

    revert previous commit

--- firebase-crashlytics/src/androidMain/kotlin/dev/gitlive/firebase/crashlytics/crashlytics.kt
+++ firebase-crashlytics/src/androidMain/kotlin/dev/gitlive/firebase/crashlytics/crashlytics.kt
@@ -10,49 +10,47 @@ public actual val Firebase.crashlytics: FirebaseCrashlytics get() =
 
 public actual fun Firebase.crashlytics(app: FirebaseApp): FirebaseCrashlytics = FirebaseCrashlytics(app.android.get(com.google.firebase.crashlytics.FirebaseCrashlytics::class.java))
 
-public actual class FirebaseCrashlytics internal constructor(internal val _android: com.google.firebase.crashlytics.FirebaseCrashlytics) {
-    public val android: com.google.firebase.crashlytics.FirebaseCrashlytics
-        get() = com.google.firebase.crashlytics.FirebaseCrashlytics.getInstance()
+public actual class FirebaseCrashlytics internal constructor(public val android: com.google.firebase.crashlytics.FirebaseCrashlytics) {
 
     public actual fun recordException(exception: Throwable) {
-        _android.recordException(exception)
+        android.recordException(exception)
     }
     public actual fun log(message: String) {
-        _android.log(message)
+        android.log(message)
     }
     public actual fun setUserId(userId: String) {
-        _android.setUserId(userId)
+        android.setUserId(userId)
     }
     public actual fun setCrashlyticsCollectionEnabled(enabled: Boolean) {
-        _android.setCrashlyticsCollectionEnabled(enabled)
+        android.setCrashlyticsCollectionEnabled(enabled)
     }
     public actual fun sendUnsentReports() {
-        _android.sendUnsentReports()
+        android.sendUnsentReports()
     }
     public actual fun deleteUnsentReports() {
-        _android.deleteUnsentReports()
+        android.deleteUnsentReports()
     }
-    public actual fun didCrashOnPreviousExecution(): Boolean = _android.didCrashOnPreviousExecution()
+    public actual fun didCrashOnPreviousExecution(): Boolean = android.didCrashOnPreviousExecution()
     public actual fun setCustomKey(key: String, value: String) {
-        _android.setCustomKey(key, value)
+        android.setCustomKey(key, value)
     }
     public actual fun setCustomKey(key: String, value: Boolean) {
-        _android.setCustomKey(key, value)
+        android.setCustomKey(key, value)
     }
     public actual fun setCustomKey(key: String, value: Double) {
-        _android.setCustomKey(key, value)
+        android.setCustomKey(key, value)
     }
     public actual fun setCustomKey(key: String, value: Float) {
-        _android.setCustomKey(key, value)
+        android.setCustomKey(key, value)
     }
     public actual fun setCustomKey(key: String, value: Int) {
-        _android.setCustomKey(key, value)
+        android.setCustomKey(key, value)
     }
     public actual fun setCustomKey(key: String, value: Long) {
-        _android.setCustomKey(key, value)
+        android.setCustomKey(key, value)
     }
     public actual fun setCustomKeys(customKeys: Map<String, Any>) {
-        _android.setCustomKeys(
+        android.setCustomKeys(
             Builder().apply {
                 customKeys.forEach { (key, value) ->
                     when (value) {
